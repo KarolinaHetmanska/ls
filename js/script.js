169367d88bb224fd768c3bee74a73d97f00cc7c2
Karolina Hetmanska <khetmanska@k85.pl>
2017-04-19 23:00:26 +0200

    ls- WIP

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-function saveData ()
+$(document).ready(function ()
 {
-localStorage.color = document.getElementById('favourite-color').value
-}
+  console.log('gotowy');
+  $('#save-values').click(function () {
+    localStorage.setItem("fruit1",$('#orange').val())
+    localStorage.name = $('#name').val()
+    console.log(localStorage.color);
+    console.log(localStorage.name);
+  })
 
-function showData()
-{
-  document.getElementById('output').innerHTML = localStorage.color
-}
+  $('#show-values').click(function () {
+    $('#output').text(localStorage.color + " " + localStorage.getItem("name"))
+    // $('#output').text + = localStorage.value
+  })
 
+  })
+// function saveData ()
+// {
+// localStorage.color = document.getElementById('favourite-color').value
+// localStorage.name = document.getElementById('name').value
+//
+// }
 
-$(document).ready(function()
-{
-  $('p').addClass('blue');
-})
+// function showData()
+// {
+//   document.getElementById('output').innerHTML = localStorage.color
+//   document.getElementById('output').innerHTML += " " + localStorage.name
+// }
