1ec97f19bd68d01ccb9af8624ed605a4ee554ea2
KarolinaHetmanska <khetmanska@k85.pl>
2017-04-20 17:22:52 +0200

    LS- WIP

--- js/script.js
+++ js/script.js
@@ -2,17 +2,38 @@ $(document).ready(function ()
 {
   console.log('gotowy');
   $('#save-values').click(function () {
-    localStorage.setItem("fruit1",$('#orange').val())
-    localStorage.name = $('#name').val()
-    console.log(localStorage.color);
+    localStorage.setItem("fruit",$('#favourite-fruit').val())
+    localStorage.name = $('#given-name').val()
+    console.log(localStorage.fruit);
     console.log(localStorage.name);
   })
 
-  $('#show-values').click(function () {
-    $('#output').text(localStorage.color + " " + localStorage.getItem("name"))
-    // $('#output').text + = localStorage.value
-  })
+  $(document).ready(function () {
+    $('#output-name').text("Cześć " + localStorage.getItem("name"))
 
+    switch (localStorage.fruit) {
+      case "maliny":
+      $('#fruit-description').text("To super, bo " + localStorage.getItem("fruit") + " są bardzo dobre");
+      break;
+
+      case "jabłka":
+      $('#fruit-description').text("To super, bo " + localStorage.getItem("fruit") + " są zdrowe");
+      break;
+
+      case "banany":
+      $('#fruit-description').text("To dobrze, bo " + localStorage.getItem("fruit") + " są smaczne i pożywne");
+      break;
+
+      case "truskawki":
+      $('#fruit-description').text("To dobrze, bo " + localStorage.getItem("fruit") + " są pyszne");
+      break;
+    }
+
+      for (var i = 1; i < 41; i+= 1 ) {
+        var randomValue = Math.random();
+        $('#fruit-items').append('<span style="top: ' + randomValue*10 +'rem; left: ' + randomValue*100 + 'rem" id="output' + i + '" class="fruit ' + localStorage.fruit +'"></span>');
+      }
+  })
   })
 // function saveData ()
 // {
